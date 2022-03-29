cae20c533f3ca4aadc46fcdf8df2f2957f9bf242
Zariphron <90808376+Zariphron@users.noreply.github.com>
2022-03-28 21:39:28 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,19 +4,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+name = "Lisa"
+city_names = ['Paris','London','Rome','Tahiti']
+class cityForm(FlaskForm):
+    cityname = StringField('City Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+form = cityForm()
+    
 @myobj.route("/", methods=['GET','POST'])
 def home():
-    user = {'username': "Lisa"}
-    cities = ['Paris','London','Rome','Tahiti']
-    class cityForm(FlaskForm):
-        cityname = StringField('City Name', validators=[DataRequired()])
-        submit = SubmitField('Submit')
-
-    form = cityForm()
     if form.validate_on_submit():
         newcity = ""
         newcity = newcity + f'{form.cityname.data}'
         if newcity is not None: 
             flash(f"{form.cityname.data}")
-            return render_template('home.html', name=user['username'], city_names=cities,form=form)
-    return render_template('home.html', name=user['username'], city_names=cities, form=form)
+            return render_template('home.html', name=name, city_names=city_names,form=form)
+    return render_template('home.html', name=name, city_names=city_names, form=form)
