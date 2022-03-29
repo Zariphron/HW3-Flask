4d13fe14901023f51de58e3a7ab43c195fdaacab
Zariphron <90808376+Zariphron@users.noreply.github.com>
2022-03-28 21:40:52 -0700

    Update home.py

--- home.py
+++ home.py
@@ -3,9 +3,9 @@ from flask import Flask
 myobj = Flask(__name__)
 
 name = "Lisa"
+city_list = ['Paris', 'London', 'Rome', 'Tahiti']
 @myobj.route("/")
 def home():
-    city_list = ['Paris', 'London', 'Rome', 'Tahiti']
     outs = ""
     for city in city_list:
         outs = outs + f'<li>{city}</li>'
